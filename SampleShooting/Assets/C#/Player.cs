df69870fe92eef869712a11a9d3026d057dc37b2
槙井 <kbc18b25@stu.kawahara.ac.jp>
2020-12-01 16:47:13 +0900

    やり直し

---
+++ SampleShooting/Assets/C#/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    AudioSource audioSource;
+    public AudioClip shotSE;
+   
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 nextPosition = transform.position + new Vector3(x, y, 0) * Time.deltaTime * 8f;
+        nextPosition = new Vector3(
+            Mathf.Clamp(nextPosition.x, -15.0f, 15.0f),
+            Mathf.Clamp(nextPosition.y, -1f, 20.0f),
+            nextPosition.z
+            );
+        transform.position = nextPosition;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            audioSource.PlayOneShot(shotSE);
+        }
+    }
+}
---
+++ SampleShooting/Assets/C#/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Animator _Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Animator = GetComponent<Animator>();
+        Sqrt2 = 1.0f / Mathf.Sqrt(2.0f);
+    }
+    float VX = 0;
+    float VY = 0;
+    float Sqrt2;
+    // Update is called once per frame
+    void Update()
+    {
+        VX = VY = 8.0f * Time.deltaTime;
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        if (x < 0) VX = -VX; else if (x == 0) VX = 0;
+        if (y < 0) VY = -VY; else if (y == 0) VY = 0;
+        // 斜め方向の速度調節
+        if (VX != 0 && VY != 0)
+        {
+            VX = VX * Sqrt2;
+            VY = VY * Sqrt2;
+        }
+        transform.position += new Vector3(VX, VY, 0);
+        _Animator.SetFloat("H", x);
+    }
+}
