81472f9a676c41a2f8dbadb5740a1a85c8c5acf8
槙井 <kbc18b25@stu.kawahara.ac.jp>
2020-12-04 09:37:28 +0900

    上げなおし

---
+++ SampleShooting/Assets/C#/CEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CEnemy : MonoBehaviour
+{
+    int Cnt = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Cnt < 80)
+        {
+            transform.position += new Vector3(0.0f, -3.0f, 0.0f) * Time.deltaTime;
+        }
+        if (Cnt > 80 + 240)
+        {
+            transform.position += new Vector3(0.0f, 3.0f, 0.0f) * Time.deltaTime;
+        }
+        Cnt++;
+        if (CUtility.IsOut(transform.position))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- SampleShooting/Assets/C#/PlayerControl.cs
+++ SampleShooting/Assets/C#/PlayerControl.cs
@@ -6,18 +6,40 @@ public class PlayerControl : MonoBehaviour
 {
     Animator _Animator;
     // Start is called before the first frame update
+    public GameObject[] ShotObjs;
     void Start()
     {
         _Animator = GetComponent<Animator>();
         Sqrt2 = 1.0f / Mathf.Sqrt(2.0f);
+        InputArray["Fire1"] = 0;
+        InputArray["Fire2"] = 0;
+        InputArray["Fire3"] = 0;
     }
     float VX = 0;
     float VY = 0;
     float Sqrt2;
+    Dictionary<string, int> InputArray = new Dictionary<string, int>();
+    void CalcInput()
+    {
+        string[] str = { "Fire1", "Fire2", "Fire3" };
+        for (int i = 0; i < str.Length; ++i)
+        {
+            if (Input.GetButton(str[i]))
+            {
+                ++InputArray[str[i]];
+            }
+            else
+            {
+                InputArray[str[i]] = 0;
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        VX = VY = 8.0f * Time.deltaTime;
+        CalcInput();
+        VX = VY = 0 < InputArray["Fire3"] ? 2.5f * Time.deltaTime : 7.0f * Time.deltaTime;
+
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
@@ -29,7 +51,54 @@ public class PlayerControl : MonoBehaviour
             VX = VX * Sqrt2;
             VY = VY * Sqrt2;
         }
-        transform.position += new Vector3(VX, VY, 0);
+        transform.position = CUtility.ClampPosition(new Vector3(transform.position.x + VX, transform.position.y + VY, 0));
+
+        // Zキー押しっぱなしで6フレームに1度ショットを発射する
+        if (0 < InputArray["Fire1"] && InputArray["Fire1"] % 6 == 0)
+        {
+            if (0 < InputArray["Fire3"])//低速移動中なら
+            {
+                LowerSpeedShot();
+            }
+            else // 通常移動中なら
+            {
+                HiSpeedShot();
+            }
+        }
         _Animator.SetFloat("H", x);
+    }
+    int[] CShot0Num = { 2, 4 };
+    Vector3[] HiSpeedShotOffsetPos =
+    {
+        new Vector3(-0.15f,0.8f),
+        new Vector3(0.15f,0.8f),
+        new Vector3(-0.45f,0.4f),
+        new Vector3(0.45f,0.4f),
+    };
+    // 通常ショット登録
+    void HiSpeedShot()
+    {
+        //	Power < 200 ?0 : 1
+        for (int i = 0; i < CShot0Num[1]; i++)
+        {
+            //SGP.Shot.Add(new CShot(X + CShot0Pos_X[i], Y + CShot0Pos_Y[i], 0.75f, 10));
+            Instantiate(ShotObjs[0], transform.position + HiSpeedShotOffsetPos[i], Quaternion.identity);
+        }
+    }
+    Vector3[] LowerSpeedShotOffsetPos =
+{
+        new Vector3(-0.05f,0.8f),
+        new Vector3(0.05f,0.8f),
+        new Vector3(-0.25f,0.4f),
+        new Vector3(0.25f,0.4f),
+    };
+    //低速通常ショット登録
+    void LowerSpeedShot()
+    {
+        for (int i = 0; i < CShot0Num[1]; i++)
+        {
+            //SGP.Shot.Add(new CShot(X + CShot0Pos_X[i], Y + CShot0Pos_Y[i], 0.75f, 10));
+            Instantiate(ShotObjs[0], transform.position + LowerSpeedShotOffsetPos[i], Quaternion.identity);
+        }
     }
 }
