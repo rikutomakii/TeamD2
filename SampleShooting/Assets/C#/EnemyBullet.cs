2e5955847e249126b179ffeebc8b41300c0e66c6
muramaruyuuki <kbc18b27@stu.kawahara.ac.jp>
2020-12-15 14:56:50 +0900

    k

--- SampleShooting/Assets/C#/EnemyBullet.cs
+++ SampleShooting/Assets/C#/EnemyBullet.cs
@@ -2,22 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy12 : MonoBehaviour
+public class EnemyBullet : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("ぶつかってる");
-        Destroy(gameObject);
     }
 }
